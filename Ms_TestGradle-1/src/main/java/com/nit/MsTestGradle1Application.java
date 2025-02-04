fd65c702b84bf50ca4ecd94b7115862c6d6011e3
Abcom <Abcom@DESKTOP-8LAP63G>
2025-02-04 14:16:54 +0530

    First Comments

---
+++ Ms_TestGradle-1/src/main/java/com/nit/MsTestGradle1Application.java
@@ -0,0 +1,15 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTestGradle1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTestGradle1Application.class, args);
+		System.out.println("hellow Everyone This The Fist Application ");
+		System.out.println("Based on the Some Git repository Cheaking purpose");
+	}
+
+}
